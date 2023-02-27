dffcec99388c35b4edf28ded555041095c3ecfbb
Tom <thomas.f.ward.89@gmail.com>
2023-02-27 17:57:00 +0000

    api call working on search button click

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const searchButton = document.getElementById("searchButton");
 const cityList = document.getElementById("cityList");
 const clearButton = document.getElementById("clearButton");
 const apiKey = "11124cc5636decd0a931bb5d68ca2f09";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
+
 
 searchButton.addEventListener("click", function() {
   const city = citySearch.value;
+  const queryUrl = `${apiUrl}?q=${city}&appid=${apiKey}`;
+
   const button = document.createElement("button");
   button.innerHTML = city;
   button.classList.add("btn", "btn-primary", "mt-3", "city-button");
@@ -18,6 +21,14 @@ searchButton.addEventListener("click", function() {
   let cities = JSON.parse(localStorage.getItem("cities")) || [];
   cities.push(city);
   localStorage.setItem("cities", JSON.stringify(cities));
+
+  //api call using ajax
+$.ajax({
+  url: queryUrl,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
 });
 
 clearButton.addEventListener("click", function() {
@@ -37,5 +48,8 @@ window.addEventListener("load", function() {
     cityList.appendChild(button);
   });
 });
+
+
+
 
 
